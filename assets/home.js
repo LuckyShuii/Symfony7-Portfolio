5958ea997709bbc24d92101bb8151ab6c9e6e1be
LuckyShuii <lucasnimes30000@gmail.com>
2024-02-22 21:26:47 +0100

    added animation on hero banner home page

--- assets/home.js
+++ assets/home.js
@@ -1,1 +1,11 @@
 import './styles/scss/home.scss';
+
+document.addEventListener('DOMContentLoaded', function () {
+    let heroImage = document.querySelector('.hero-image');
+    let a = document.querySelector('.contact-hero-btn');
+
+    window.addEventListener('load', function () {
+        heroImage.classList.add('show');
+        a.classList.add('show');
+    });
+});
